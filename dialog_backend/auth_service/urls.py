8efe5bc5e52cfc859a25cac967981b95d9ea7c67
alex_barmen <www.lad282@gmail.com>
2025-02-07 22:22:27 +0300

    feat: moved token urls to auth_service app

--- dialog_backend/auth_service/urls.py
+++ dialog_backend/auth_service/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from auth_service.views import RegistrationAPIView, LoginAPIView, LogoutAPIView
 
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegistrationAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
