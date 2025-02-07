51fbcec3b794e37514b18f7bad2e1a5570dbae1e
alex_barmen <www.lad282@gmail.com>
2025-02-07 23:01:44 +0300

    feat: UserProfile model and migration to it

--- dialog_backend/cabinet/models.py
+++ dialog_backend/cabinet/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from core.mixins import AutoDateMixin
+from core.validators import validate_not_future_date
+
+
+class UserProfile(AutoDateMixin):
+    """Модель профиля пользователя"""
+    
+    MALE = 'MALE'
+    FEMALE = 'FEMALE'
+    GENDER_CHOICES = {
+        MALE: 'Мужской',
+        FEMALE: 'Женский',
+    }
+    
+    DIABETES_TYPE_CHOICES = {
+        '1': '1',
+        '2': '2',
+    }
+    
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        help_text='Пользователь, к которому относится данный профиль',
+    )
+    patronymic_name = models.CharField(
+        max_length=150,
+        default='',
+        verbose_name='Отчество',
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES,
+        verbose_name='Пол',
+    )
+    birth_date = models.DateField(
+        verbose_name='Дата рождения',
+        validators=[validate_not_future_date],
+    )
+    diabetes_type = models.CharField(
+        max_length=5,
+        choices=DIABETES_TYPE_CHOICES,
+        verbose_name='Тип диабета',
+    )
+    diagnosis_date = models.DateField(
+        verbose_name='Дата постановки диагноза',
+        validators=[validate_not_future_date],
+        null=True,
+        blank=True,
+    )
+    
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
+        ordering = ('updated_at',)
+    
+    def __str__(self):
+        return f'Профиль пользователя {self.user}'
