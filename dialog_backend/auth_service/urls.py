93c7e801d09b47bac95489dd8d07b06828a9c150
alex_barmen <www.lad282@gmail.com>
2025-02-07 22:03:09 +0300

    feat: urls for auth_service

---
+++ dialog_backend/auth_service/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from auth_service.views import RegistrationAPIView, LoginAPIView, LogoutAPIView
+
+
+urlpatterns = [
+    path('register/', RegistrationAPIView.as_view(), name='register'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+]
--- dialog_backend/dialog_backend/urls.py
+++ dialog_backend/dialog_backend/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('api/auth_service/', include('auth_service.urls')),
 ]
