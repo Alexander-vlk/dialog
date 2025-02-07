665dcc4ca2e7ec8d57f3fb2655a1c09dc60fdb60
alex_barmen <www.lad282@gmail.com>
2025-02-07 23:01:59 +0300

    feat: UserProfile registered to admin panel

--- dialog_backend/cabinet/admin.py
+++ dialog_backend/cabinet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from cabinet.models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """Админ для модели UserProfile"""
+    
+    list_display = ['user__username', 'gender', 'diabetes_type', 'updated_at']
+    raw_id_fields = ['user']
+    
