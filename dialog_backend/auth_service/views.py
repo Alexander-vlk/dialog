f2f38ed4bdd11b3386fd2b845118e27b820ebbc4
alex_barmen <www.lad282@gmail.com>
2025-02-07 21:17:50 +0300

    feat: login view

--- dialog_backend/auth_service/views.py
+++ dialog_backend/auth_service/views.py
@@ -1,3 +1,4 @@
+from rest_framework.authentication import authenticate
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -28,3 +29,46 @@ class RegistrationAPIView(APIView):
                 },
                 status=status.HTTP_201_CREATED,
             )
+
+
+class LoginAPIView(APIView):
+    """APIView для аутентификации пользователя"""
+    
+    def post(self, request):
+        data = request.data
+        
+        username = data.get('username', None)
+        password = data.get('password', None)
+        
+        if not username or not password:
+            return Response(
+                {
+                    'error': 'Оба поля должны быть заполнены',
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        user = authenticate(username=username, password=password)
+        
+        if not user:
+            return Response(
+                {
+                    'error': 'Неверные имя пользователя или пароль',
+                },
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+            
+        refresh = RefreshToken.for_user(user)
+        
+        refresh.payload.update({
+            'user_id': user.id,
+            'username': user.username,
+        })
+        
+        return Response(
+            {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            },
+            status=status.HTTP_200_OK,
+        )
