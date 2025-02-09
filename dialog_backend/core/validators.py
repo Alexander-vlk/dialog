c249935b15e4ca66d37cf121aedd24d0de27dd96
alex_barmen <www.lad282@gmail.com>
2025-02-09 17:25:25 +0300

    feat: validate_positive_float validator

--- dialog_backend/core/validators.py
+++ dialog_backend/core/validators.py
@@ -7,3 +7,10 @@ def validate_not_future_date(validate_date):
     """Проверяет, что дата не в будущем"""
     if validate_date > date.today():
         raise ValidationError('Дата не может быть больше текущей')
+
+
+def validate_positive_float(validate_number: float):
+    """Проверяет, что число неотрицательное"""
+
+    if validate_number < 0:
+        raise ValidationError('Данный показатель не может быть отрицательным')
