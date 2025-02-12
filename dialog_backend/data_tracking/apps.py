e3e0911b688ccd1108f9b24c3d96015f6d5acb89
alex_barmen <www.lad282@gmail.com>
2025-02-12 18:36:48 +0300

    feat: verbose_name for data_tracking app

--- dialog_backend/data_tracking/apps.py
+++ dialog_backend/data_tracking/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DataTrackingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data_tracking'
+    verbose_name = 'Отслеживание состояния'
