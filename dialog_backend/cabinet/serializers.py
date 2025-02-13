f21c8b147f1ec1ba1e5c3cceab137206413fda4a
alex_barmen <www.lad282@gmail.com>
2025-02-13 19:11:52 +0300

    refactor: disease serializer

--- dialog_backend/cabinet/serializers.py
+++ dialog_backend/cabinet/serializers.py
@@ -8,9 +8,22 @@ class AllergySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Allergy
-        fields = ('name', )
+        fields = ('name',)
 
     def create(self, validated_data):
         allergy = super().create(validated_data)
         allergy.users.add(self.context['request'].user)
         return allergy
+
+
+class DiseaseSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Disease"""
+
+    class Meta:
+        model = Disease
+        fields = ('name',)
+
+    def create(self, validated_data):
+        disease = super().create(validated_data)
+        disease.users.add(self.context['request'].user)
+        return disease
