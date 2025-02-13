fd6e518b8021fd07027835ae3a84d1ecf964bcfa
alex_barmen <www.lad282@gmail.com>
2025-02-13 20:09:53 +0300

    feat: slash to urls

--- dialog_backend/dialog_backend/urls.py
+++ dialog_backend/dialog_backend/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth_service/', include('auth_service.urls')),
-    path('api/cabinet', include('cabinet.urls')),
+    path('api/cabinet/', include('cabinet.urls')),
 ]
