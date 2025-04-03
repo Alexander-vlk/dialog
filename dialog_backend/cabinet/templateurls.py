4fa2e6a2a8a15b659ecfb9e16f7109612be92d21
alex_barmen <www.lad282@gmail.com>
2025-04-03 21:15:23 +0300

    feat: edit profile views

--- dialog_backend/cabinet/templateurls.py
+++ dialog_backend/cabinet/templateurls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from cabinet.templateviews import index, cabinet
+from cabinet.templateviews import index, cabinet, edit_profile
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('cabinet/', cabinet, name='cabinet'),
+
+    path('cabinet/update', edit_profile, name='cabinet_update'),
 ]
--- dialog_backend/cabinet/templateviews.py
+++ dialog_backend/cabinet/templateviews.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
-from django.shortcuts import render
+from django.http import HttpResponseBadRequest
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
+from django.views.generic import UpdateView
+
+from cabinet.forms import UserProfileEditForm
+from cabinet.models import UserProfile
 
 
 def index(request):
@@ -16,3 +22,30 @@ def cabinet(request):
     }
 
     return render(request, 'cabinet/cabinet.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+@login_required
+def edit_profile(request):
+    """View редактирования профиля"""
+    if request.method == 'POST':
+        form = UserProfileEditForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if form.is_valid():
+            form.save()
+            request.user.username = form.cleaned_data['username']
+            request.user.email = form.cleaned_data['email']
+            request.user.first_name = form.cleaned_data['first_name']
+            request.user.last_name = form.cleaned_data['last_name']
+            request.user.save()
+
+            cabinet_url = reverse('cabinet')
+            return redirect(f'{cabinet_url}?success_edit_profile=true')
+
+    form = UserProfileEditForm(instance=request.user.userprofile)
+
+    template = 'cabinet/edit_profile.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
+
