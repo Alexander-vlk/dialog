27c5225f18cee4410de4ca071c5790bace18ee05
alex_barmen <www.lad282@gmail.com>
2025-04-05 10:01:44 +0300

    feat: register rate and advantage in admin page

--- dialog_backend/cabinet/admin.py
+++ dialog_backend/cabinet/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from cabinet.models import Allergy, Disease, UserProfile
+from cabinet.models import Advantage, Allergy, Disease, UserProfile, Rate
+
+
+@admin.register(Advantage)
+class AdvantageAdmin(admin.ModelAdmin):
+    """Админ для модели Advantage"""
+
+    list_display = ['order_num', 'title', 'description']
+    list_display_links = ['title']
+
 
 
 @admin.register(Allergy)
@@ -23,3 +32,13 @@ class UserProfileAdmin(admin.ModelAdmin):
     
     list_display = ['user__username', 'gender', 'diabetes_type', 'updated_at']
     raw_id_fields = ['user']
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_info', 'text', 'is_visible']
+    list_display_links = ['id', 'user_info']
+
+    @admin.display(description='Пользователь')
+    def user_info(self, obj):
+        return obj.user_info
