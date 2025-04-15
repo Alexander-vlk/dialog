3a7bfa8ffb60c19746ec2af4055b8f8c7c3b9e58
alex_barmen <www.lad282@gmail.com>
2025-04-15 20:11:27 +0300

    refactor: urls for api views

--- dialog_backend/data_tracking/urls.py
+++ dialog_backend/data_tracking/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from data_tracking.views.private import PressureAPIView
-
+from data_tracking.views.private import BodyTemperatureAPIView, GlucoseAPIView, PressureAPIView
 
 urlpatterns = [
+    path('body-temperature/', BodyTemperatureAPIView.as_view(), name='body-temperature'),
+    path('glucose/', GlucoseAPIView.as_view(), name='glucose'),
     path('pressure/', PressureAPIView.as_view(), name='pressure'),
 ]
