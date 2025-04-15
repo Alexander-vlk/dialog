2e333c0cd0448385c3d636b4fbd971ec92d4b5ee
alex_barmen <www.lad282@gmail.com>
2025-04-15 22:12:06 +0300

    refactor: use _ instead of -

--- dialog_backend/data_tracking/urls.py
+++ dialog_backend/data_tracking/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from data_tracking.views.private import BodyTemperatureAPIView, GlucoseAPIView, PressureAPIView
 
 urlpatterns = [
-    path('body-temperature/', BodyTemperatureAPIView.as_view(), name='body-temperature'),
+    path('body-temperature/', BodyTemperatureAPIView.as_view(), name='body_temperature'),
     path('glucose/', GlucoseAPIView.as_view(), name='glucose'),
     path('pressure/', PressureAPIView.as_view(), name='pressure'),
 ]
