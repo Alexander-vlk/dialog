50718b059f1a816316daeacb269c1f0ff2f171bf
alex_barmen <www.lad282@gmail.com>
2025-04-16 10:29:27 +0300

    refactor: remove unused views and imports

--- dialog_backend/data_tracking/templateurls.py
+++ dialog_backend/data_tracking/templateurls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 
 from data_tracking.templateviews import (
-    glucose,
-    pressure,
-    temperature,
     daily_log,
     DailyLogListView,
 )
 
 urlpatterns = [
-    path('glucose/new', glucose, name='new_glucose'),
-    path('pressure/new', pressure, name='new_pressure'),
-    path('temperature/new', temperature, name='new_temperature'),
 
     path('daily_log', daily_log, name='daily_log'),
     path('daily_log_list', DailyLogListView.as_view(), name='daily_log_list'),
--- dialog_backend/data_tracking/templateviews.py
+++ dialog_backend/data_tracking/templateviews.py
@@ -1,71 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count, Avg
-from django.http import HttpResponseBadRequest, JsonResponse
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils import timezone
-from django.views.decorators.http import require_POST, require_http_methods, require_GET
+from django.views.decorators.http import require_http_methods
 from django.views.generic import ListView
 
-from data_tracking.forms import GlucoseForm, BodyTemperatureForm, PressureForm, DailyLogForm
-from data_tracking.models import DailyLog, Glucose, Pressure, BodyTemperature
-
-
-@login_required
-@require_POST
-def glucose(request):
-    """Сохранение замеров глюкозы"""
-    form = GlucoseForm(request.POST)
-
-    if not form.is_valid():
-        return HttpResponseBadRequest()
-
-    daily_log = DailyLog.objects.get(user=request.user, date=timezone.now())
-
-    glucose_instance = form.save(commit=False)
-    glucose_instance.user = request.user
-    glucose_instance.daily_log = daily_log
-    glucose_instance.save()
-
-    return JsonResponse({'success': True})
-
-
-@login_required
-@require_POST
-def pressure(request):
-    """Сохранение замеров давления"""
-    form = PressureForm(request.POST)
-
-    if not form.is_valid():
-        return HttpResponseBadRequest()
-
-    daily_log = DailyLog.objects.get(user=request.user, date=timezone.now())
-
-    pressure_instance = form.save(commit=False)
-    pressure_instance.user = request.user
-    pressure_instance.daily_log = daily_log
-    pressure_instance.save()
-
-    return JsonResponse({'success': True})
-
-
-@login_required
-@require_POST
-def temperature(request):
-    """Сохранение замеров температуры"""
-    form = BodyTemperatureForm(request.POST)
-
-    if not form.is_valid():
-        return HttpResponseBadRequest()
-
-    daily_log = DailyLog.objects.get(user=request.user, date=timezone.now())
-
-    temperature_instance = form.save(commit=False)
-    temperature_instance.user = request.user
-    temperature_instance.daily_log = daily_log
-    temperature_instance.save()
-
-    return JsonResponse({'success': True})
+from data_tracking.forms import DailyLogForm
+from data_tracking.models import DailyLog
 
 
 @login_required
