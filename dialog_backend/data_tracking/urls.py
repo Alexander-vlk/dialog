6cc1c65c12d6bf03bfb9e832b3947f4f8758ba57
alex_barmen <www.lad282@gmail.com>
2025-04-16 18:21:22 +0300

    feat: weekly log api view in url patterns

--- dialog_backend/data_tracking/urls.py
+++ dialog_backend/data_tracking/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from data_tracking.views.private import BodyTemperatureAPIView, GlucoseAPIView, PressureAPIView
+from data_tracking.views.private import (
+    BodyTemperatureAPIView,
+    GlucoseAPIView,
+    PressureAPIView,
+    WeeklyLogAPIView,
+)
 
 urlpatterns = [
     path('body-temperature/', BodyTemperatureAPIView.as_view(), name='body_temperature'),
     path('glucose/', GlucoseAPIView.as_view(), name='glucose'),
     path('pressure/', PressureAPIView.as_view(), name='pressure'),
+
+    path('weekly-log/', WeeklyLogAPIView.as_view(), name='weekly_log'),
 ]
