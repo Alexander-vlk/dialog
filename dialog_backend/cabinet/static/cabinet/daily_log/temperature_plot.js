2a0707c85464e948bfc7464b5b193a94df088742
alex_barmen <www.lad282@gmail.com>
2025-04-17 21:52:10 +0300

    refactor: whitespace in the end of files

--- dialog_backend/cabinet/static/cabinet/daily_log/temperature_plot.js
+++ dialog_backend/cabinet/static/cabinet/daily_log/temperature_plot.js
@@ -21,31 +21,31 @@ const getTemperatureData = async () => {
     return result
 }
 
-    const tempChart = document.getElementById('tempChart')
-    const tempLoader = document.getElementById('tempChart-loader')
-    if (tempChart) {
-        const {labels, temperatureData} = await getTemperatureData();
-        new Chart(tempChart, {
-            ...baseOptions,
-            data: {
-                labels,
-                datasets: [{
-                    label: 'Температура (°C)',
-                    data: temperatureData,
-                    borderColor: '#60a5fa',
-                    backgroundColor: '#bfdbfe',
-                    tension: 0.3,
-                    fill: false
-                }]
-            },
-            options: {
-                responsive: true,
-                animation: {
-                    duration: 1000,
-                    onComplete: () => {
-                        tempLoader.style.display = 'none'
-                    }
+const tempChart = document.getElementById('tempChart')
+const tempLoader = document.getElementById('tempChart-loader')
+if (tempChart) {
+    const {labels, temperatureData} = await getTemperatureData();
+    new Chart(tempChart, {
+        ...baseOptions,
+        data: {
+            labels,
+            datasets: [{
+                label: 'Температура (°C)',
+                data: temperatureData,
+                borderColor: '#60a5fa',
+                backgroundColor: '#bfdbfe',
+                tension: 0.3,
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            animation: {
+                duration: 1000,
+                onComplete: () => {
+                    tempLoader.style.display = 'none'
                 }
             }
-        });
-    }
+        }
+    });
+}
