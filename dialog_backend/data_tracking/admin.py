5f68be3477bbbe2af5b1d429161cb9bb66569602
alex_barmen <www.lad282@gmail.com>
2025-04-27 17:16:23 +0300

    feat: update django admin

--- dialog_backend/data_tracking/admin.py
+++ dialog_backend/data_tracking/admin.py
@@ -15,16 +15,18 @@ from data_tracking.models import (
 class MonthlyLogAdmin(admin.ModelAdmin):
     """Админ для MonthlyLog"""
 
-    list_display = ('user', 'created_at', 'month')
+    list_display = ('user', 'created_at', 'month', 'year')
     raw_id_fields = ('user',)
+    list_filter = ('user', 'month', 'year')
 
 
 @admin.register(WeeklyLog)
 class WeeklyLogAdmin(admin.ModelAdmin):
     """Админ для WeeklyLog"""
 
-    list_display = ('user', 'monthly_log', 'week_start', 'week_end', 'created_at')
+    list_display = ('user', 'week_start', 'week_end', 'monthly_log', 'created_at')
     raw_id_fields = ('user', 'monthly_log')
+    list_filter = ('user', 'monthly_log')
 
 
 @admin.register(Health)
@@ -39,6 +41,7 @@ class DailyLogAdmin(admin.ModelAdmin):
 
     list_display = ('user', 'weekly_log', 'date')
     raw_id_fields = ('user', 'weekly_log')
+    list_filter = ('user', 'weekly_log', 'date')
 
 
 @admin.register(BodyTemperature)
