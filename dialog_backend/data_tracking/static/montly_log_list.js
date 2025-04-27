8f98ec44845f6854bfbd5b3eb25637bb47b02737
alex_barmen <www.lad282@gmail.com>
2025-04-27 21:48:36 +0300

    feat: monthly log list js file

---
+++ dialog_backend/data_tracking/static/montly_log_list.js
@@ -0,0 +1,13 @@
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('-translate-x-full');
+    overlay.classList.toggle('hidden');
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.add('-translate-x-full');
+    overlay.classList.add('hidden');
+});
