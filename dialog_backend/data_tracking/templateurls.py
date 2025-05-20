640650346106aff3b6586e6a00bd2073e7cc77dd
alex_barmen <www.lad282@gmail.com>
2025-05-20 18:53:21 +0300

    feat: daily log detail page view in urls

--- dialog_backend/data_tracking/templateurls.py
+++ dialog_backend/data_tracking/templateurls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from data_tracking.templateviews import (
     daily_log,
     DailyLogListView,
+    DailyLogDetailView,
     edit_monthly_log,
     monthly_log_list,
     weekly_log,
@@ -14,6 +15,7 @@ urlpatterns = [
     path('daily_log', daily_log, name='daily_log'),
     path('weekly_log', weekly_log, name='weekly_log'),
     path('daily_log_list', DailyLogListView.as_view(), name='daily_log_list'),
+    path('daily_log/<int:pk>/detail', DailyLogDetailView.as_view(), name='daily_log_detail'),
     path('weekly_log_list', WeeklyLogListView.as_view(), name='weekly_log_list'),
     path('monthly_logs/<int:monthly_log_id>', monthly_log_list, name='monthly_log'),
     path('monthly_logs/<int:monthly_log_id>/edit', edit_monthly_log, name='monthly_log_edit'),
