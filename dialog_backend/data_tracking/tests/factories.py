cf0db37a423d4bf20589d0431367ca2ae76a63c4
alex_barmen <www.lad282@gmail.com>
2025-05-30 17:59:28 +0300

    feat: extend weekly log factory

--- dialog_backend/data_tracking/tests/factories.py
+++ dialog_backend/data_tracking/tests/factories.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from factory.fuzzy import FuzzyFloat
 from factory.django import DjangoModelFactory
 
 from data_tracking.models import MonthlyLog, WeeklyLog
@@ -18,3 +19,7 @@ class WeeklyLogFactory(DjangoModelFactory):
 
     class Meta:
         model = WeeklyLog
+
+    weight = FuzzyFloat(0)
+    bmi = FuzzyFloat(0)
+    ketones = FuzzyFloat(0)
