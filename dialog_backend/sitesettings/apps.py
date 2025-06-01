c2836db94e973b57304dbc9674ab7f5600c1893b
alex_barmen <www.lad282@gmail.com>
2025-06-01 11:07:14 +0300

    feat: sitesettings app

---
+++ dialog_backend/sitesettings/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class SitesettingsConfig(AppConfig):
+    """Конфигурация приложения"""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sitesettings'
+    verbose_name = 'Настройки сайта'
