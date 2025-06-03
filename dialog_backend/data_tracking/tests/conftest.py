e2848cf9b41c8488db50a130e2fb9f7309638be0
alex_barmen <www.lad282@gmail.com>
2025-06-03 20:54:01 +0300

    feat: daily log and user with daily log fixtures

--- dialog_backend/data_tracking/tests/conftest.py
+++ dialog_backend/data_tracking/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from data_tracking.tests.factories import MonthlyLogFactory, WeeklyLogFactory
+from data_tracking.tests.factories import DailyLogFactory, MonthlyLogFactory, WeeklyLogFactory
 
 
 @pytest.fixture
@@ -13,6 +13,12 @@ def monthly_log(user):
 def weekly_log(user, monthly_log):
     """Фикстура еженедельного отчета, привязанного к ежемесячному отчету"""
     return WeeklyLogFactory(user=user, monthly_log=monthly_log)
+
+
+@pytest.fixture
+def daily_log(user, weekly_log):
+    """Фикстура ежедневного отчета, привязанного к недельному отчету"""
+    return DailyLogFactory(user=user, weekly_log=weekly_log)
 
 
 @pytest.fixture
@@ -29,4 +35,12 @@ def user_with_weekly_log(user, weekly_log):
     """Фикстура пользователя с актуальным еженедельным отчетом"""
     weekly_log.user = user
     weekly_log.save()
+    return user
+
+
+@pytest.fixture
+def user_with_daily_log(user, daily_log):
+    """Фикстура пользователя с актуальным ежедневным отчетом"""
+    daily_log.user = user
+    daily_log.save()
     return user
