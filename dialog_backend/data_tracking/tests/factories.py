67d3f66b15147fc7d50149cd7f347cb0a4d2c21c
alex_barmen <www.lad282@gmail.com>
2025-06-03 20:47:55 +0300

    feat: daily log factory

--- dialog_backend/data_tracking/tests/factories.py
+++ dialog_backend/data_tracking/tests/factories.py
@@ -1,8 +1,8 @@
 from django.utils import timezone
-from factory.fuzzy import FuzzyFloat
+from factory.fuzzy import FuzzyFloat, FuzzyInteger
 from factory.django import DjangoModelFactory
 
-from data_tracking.models import MonthlyLog, WeeklyLog
+from data_tracking.models import DailyLog, MonthlyLog, WeeklyLog
 
 
 class MonthlyLogFactory(DjangoModelFactory):
@@ -23,3 +23,18 @@ class WeeklyLogFactory(DjangoModelFactory):
     weight = FuzzyFloat(0)
     bmi = FuzzyFloat(0)
     ketones = FuzzyFloat(0)
+
+
+class DailyLogFactory(DjangoModelFactory):
+    """Фабрика модели DailyLog"""
+
+    class Meta:
+        model = DailyLog
+
+    calories_count = FuzzyInteger(500, 4500)
+
+    proteins_count = FuzzyInteger(20, 200)
+    fats_count = FuzzyInteger(20, 200)
+    carbs_count = FuzzyInteger(20, 200)
+
+    date = timezone.now().date
