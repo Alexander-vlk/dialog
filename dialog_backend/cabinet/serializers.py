d2ebe12adaa0406f1158c42ab31d155e46b047b8
alex_barmen <www.lad282@gmail.com>
2025-06-04 19:59:38 +0300

    refactor(users api): rate and advantage serializers

--- dialog_backend/cabinet/serializers.py
+++ dialog_backend/cabinet/serializers.py
@@ -1,6 +1,7 @@
+from drf_spectacular.utils import extend_schema_serializer, OpenApiExample
 from rest_framework import serializers
 
-from cabinet.models import Allergy, Disease, UserProfile
+from cabinet.models import Advantage, Allergy, Disease, Rate, UserProfile
 
 
 class AllergySerializer(serializers.ModelSerializer):
@@ -58,3 +59,71 @@ class UserProfileSerializer(serializers.ModelSerializer):
             user_profile.user = user
         user_profile.save()
         return user_profile
+
+
+@extend_schema_serializer(
+    many=True,
+    examples=[
+        OpenApiExample(
+            'Пример ответа от сервера',
+            description='Базовый ответ',
+            value=[
+                {
+                    'title': 'Преимущество 1',
+                    'description': 'Описание преимущества',
+                    'image_url': 'https://dialog.com/media/advantages/advantage1.png',
+                    'order_num': 1,
+                },
+            ],
+        ),
+    ],
+)
+class AdvantageSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Advantage"""
+
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Advantage
+        fields = ('title', 'description', 'image_url', 'order_num')
+
+    def get_image_url(self, obj):
+        """Получить url изображения"""
+        return obj.image.url if obj.image else ''
+
+
+@extend_schema_serializer(
+    many=True,
+    examples=[
+        OpenApiExample(
+            'Пример ответа от сервера',
+            description='Базовый ответ',
+            value=[
+                {
+                    'user_info': 'Тестина Тестова',
+                    'text': 'Текст отзыва',
+                },
+                {
+                    'user_info': 'Тестина',
+                    'text': 'Текст отзыва',
+                },
+            ],
+        ),
+        OpenApiExample(
+            'Анонимный отзыв',
+            description='Пример ответа от сервера, если отзыв оставлен анонимным пользователем',
+            value=[
+                {
+                    'user_info': 'Аноним',
+                    'text': 'Текст отзыва',
+                },
+            ],
+        ),
+    ],
+)
+class RateSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Rate"""
+
+    class Meta:
+        model = Rate
+        fields = ('user_info', 'text')
