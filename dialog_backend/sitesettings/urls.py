5b5bb9367887533b5562386d5d0337a17b0e164e
alex_barmen <www.lad282@gmail.com>
2025-06-04 19:23:22 +0300

    refactor(site settings views): rename url

--- dialog_backend/sitesettings/urls.py
+++ dialog_backend/sitesettings/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('call-action-block/', CallToActionBlockView.as_view(), name='call_action_block'),
     path('features/', FeatureAPIView.as_view(), name='features'),
     path('hero-action-block/', HeroActionBlockAPIView.as_view(), name='hero_action_block'),
-    path('main-page-faq/', MainPageFAQAPIView.as_view(), name='main_page_faq'),
+    path('main-page-faqs/', MainPageFAQAPIView.as_view(), name='main_page_faqs'),
     path('slider-images/', SliderImageAPIView.as_view(), name='slider_images'),
 ]
