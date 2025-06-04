f4c0f3a45a4cad010288793f379d2c6af8d090f2
alex_barmen <www.lad282@gmail.com>
2025-06-04 17:35:43 +0300

    feat(site settings views): FeatureAPIView and fix CallToActionBlockView

--- dialog_backend/sitesettings/urls.py
+++ dialog_backend/sitesettings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from sitesettings.views.public import CallToActionBlockView
+from sitesettings.views.public import CallToActionBlockView, FeatureAPIView
 
 
 urlpatterns = [
     path('call-action-block/', CallToActionBlockView.as_view(), name='call_action_block'),
+    path('features/', FeatureAPIView.as_view(), name='features'),
 ]
--- dialog_backend/sitesettings/views/public.py
+++ dialog_backend/sitesettings/views/public.py
@@ -1,16 +1,20 @@
+from django.core.cache import cache
 from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
 from constants import SWAGGER_ERROR_MESSAGES
-from sitesettings.constants import MAIN_PAGE_DATA_TAG
+from sitesettings.constants import MAIN_PAGE_DATA_TAG, FEATURES_CACHE_KEY
 from sitesettings.models.main_page import (
     CallToActionBlock,
+    Feature,
 )
 from sitesettings.serializers import (
     CallToActionBlockSerializer,
+    FeatureSerializer,
 )
+from sitesettings.utils import get_main_page_settings
 
 
 @extend_schema(
@@ -31,11 +35,42 @@ class CallToActionBlockView(APIView):
 
     def get(self, request, *args, **kwargs):
         """GET-запрос"""
-        serializer = self.serializer_class(data=self.get_queryset(), many=True)
-        serializer.is_valid(raise_exception=True)
+        serializer = self.serializer_class(instance=self.get_queryset(), many=True)
 
-        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def get_queryset(self):
         """Получить объект блока призыва к действию"""
         return CallToActionBlock.objects.filter(show_on_main_page=True)
+
+
+@extend_schema(
+    tags=[MAIN_PAGE_DATA_TAG],
+    methods=['GET'],
+    responses={
+        status.HTTP_200_OK: FeatureSerializer,
+        **SWAGGER_ERROR_MESSAGES,
+    }
+)
+class FeatureAPIView(APIView):
+    """APIView для получения данных о функционале приложения"""
+
+    authentication_classes: list = []
+    permission_classes: list = []
+
+    serializer_class = FeatureSerializer
+
+    def get(self, request, *args, **kwargs):
+        """GET-запрос"""
+        serializer = self.serializer_class(instance=self.get_queryset(), many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_queryset(self):
+        """Получение queryset-а"""
+        features_queryset = cache.get(FEATURES_CACHE_KEY)
+        if not features_queryset:
+            features_queryset = Feature.objects.all()[:get_main_page_settings().max_functions_count]
+            cache.set(FEATURES_CACHE_KEY, features_queryset, 10000)
+
+        return features_queryset
