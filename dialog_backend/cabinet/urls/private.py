713c8d728890a907f64a89dd7e0a2e01c62eb18e
alex_barmen <www.lad282@gmail.com>
2025-06-16 16:29:34 +0300

    feat: register api endpoint in urls

--- dialog_backend/cabinet/urls/private.py
+++ dialog_backend/cabinet/urls/private.py
@@ -1,12 +1,15 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from cabinet.views.private import UserViewSet
+from cabinet.views.private import UserViewSet, UserDataAPIView
 
 
 router = SimpleRouter()
 router.register(r'users', UserViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path('users/me', UserDataAPIView.as_view(), name='current_user_data'),
+]
 
 urlpatterns.extend(router.urls)
