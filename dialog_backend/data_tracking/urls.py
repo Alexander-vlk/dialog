65948a2079607f5956af25a117920e23b456a489
alex_barmen <www.lad282@gmail.com>
2025-09-18 19:31:55 +0300

    feat: mood api view

--- dialog_backend/data_tracking/urls.py
+++ dialog_backend/data_tracking/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from data_tracking.views import MoodAPIView
 from data_tracking.views.private import (
     AvgBJUApiView,
     AverageGlucoseDataAPIView,
@@ -13,15 +14,16 @@ from data_tracking.views.private import (
 
 urlpatterns = [
     path(
-        "glucose/average/", AverageGlucoseDataAPIView.as_view(), name="average-glucose"
+        'glucose/average/', AverageGlucoseDataAPIView.as_view(), name='average-glucose'
     ),
     path(
-        "body-temperature/", BodyTemperatureAPIView.as_view(), name="body_temperature"
+        'body-temperature/', BodyTemperatureAPIView.as_view(), name='body_temperature'
     ),
-    path("calories/", CaloriesAPIView.as_view(), name="calories"),
-    path("glucose/", GlucoseAPIView.as_view(), name="glucose"),
-    path("pressure/", PressureAPIView.as_view(), name="pressure"),
-    path("bju/average/", AvgBJUApiView.as_view(), name="bju-average"),
-    path("health/", HealthAPIView.as_view(), name="health"),
-    path("weekly-log/", WeeklyLogAPIView.as_view(), name="weekly_log"),
+    path('calories/', CaloriesAPIView.as_view(), name='calories'),
+    path('glucose/', GlucoseAPIView.as_view(), name='glucose'),
+    path('pressure/', PressureAPIView.as_view(), name='pressure'),
+    path('bju/average/', AvgBJUApiView.as_view(), name='bju-average'),
+    path('health/', HealthAPIView.as_view(), name='health'),
+    path('mood/', MoodAPIView.as_view(), name='mood'),
+    path('weekly-log/', WeeklyLogAPIView.as_view(), name='weekly_log'),
 ]
--- dialog_backend/data_tracking/views/public.py
+++ dialog_backend/data_tracking/views/public.py
@@ -1,17 +1,19 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from constants import SWAGGER_ERROR_MESSAGES, MOOD_SWAGGER_TAG
-from data_tracking.serializers import HealthSerializer
+from constants import SWAGGER_ERROR_MESSAGES, HEALTH_SWAGGER_TAG
+from data_tracking.models import Mood
+from data_tracking.serializers import MoodSerializer
 
 
 @extend_schema(
-    tags=[MOOD_SWAGGER_TAG],
+    tags=[HEALTH_SWAGGER_TAG],
     methods=["GET"],
     description='Справочник - виды самочувствия',
     responses={
-        status.HTTP_200_OK: HealthSerializer,
+        status.HTTP_200_OK: MoodSerializer,
         **SWAGGER_ERROR_MESSAGES,
     },
 )
@@ -21,7 +23,9 @@ class MoodAPIView(APIView):
     authentication_classes: list = []
     permission_classes: list = []
 
-    serializer_class = HealthSerializer
+    serializer_class = MoodSerializer
 
     def get(self, request):
         """GET-запрос"""
+        serializer = self.serializer_class(instance=Mood.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
