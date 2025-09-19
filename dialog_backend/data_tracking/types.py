c973a1dd0e8bf6ea78ffbc4eba31e3f42e27fc68
alex_barmen <www.lad282@gmail.com>
2025-09-19 21:44:14 +0300

    feat: daily log data type

---
+++ dialog_backend/data_tracking/types.py
@@ -0,0 +1,19 @@
+import datetime
+
+from typing_extensions import TypedDict, NotRequired
+
+
+class DailyLogData(TypedDict):
+    """Типизация для DailyLog"""
+
+    user_id: int
+    weekly_log_id: int
+    calories_count: int
+    proteins_count: int
+    fats_count: int
+    carbs_count: int
+    mood: int
+    health_ids: NotRequired[list[int] | None]
+    physical_activity: str
+    additional_info: str
+    date: datetime.date
