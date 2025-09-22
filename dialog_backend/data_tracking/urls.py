82e001164f639f6808abc8a0f0a753e001a21e5c
alex_barmen <www.lad282@gmail.com>
2025-09-22 18:58:39 +0300

    refactor

--- dialog_backend/data_tracking/urls.py
+++ dialog_backend/data_tracking/urls.py
@@ -15,12 +15,12 @@ from data_tracking.views.private import (
 )
 
 urlpatterns = [
-    path('glucose/average/', AverageGlucoseDataAPIView.as_view(), name='average-glucose'),
+    path('glucose/average/', AverageGlucoseDataAPIView.as_view(), name='average_glucose'),
     path('body-temperature/', BodyTemperatureAPIView.as_view(), name='body_temperature'),
     path('calories/', CaloriesAPIView.as_view(), name='calories'),
     path('glucose/', GlucoseAPIView.as_view(), name='glucose'),
     path('pressure/', PressureAPIView.as_view(), name='pressure'),
-    path('bju/average/', AvgBJUApiView.as_view(), name='bju-average'),
+    path('bju/average/', AvgBJUApiView.as_view(), name='average_bju'),
     path('health/', HealthAPIView.as_view(), name='health'),
     path('mood/', MoodAPIView.as_view(), name='mood'),
     path('daily-logs/current/', DailyLogAPIView.as_view(), name='current_daily_log'),
