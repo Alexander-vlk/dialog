bd0e816aded56ea18879ff58038ea9a536e4b63f
alex_barmen <www.lad282@gmail.com>
2026-03-07 17:22:48 +0300

    feat: remove date filter

--- dialog_backend/data_tracking/viewsets.py
+++ dialog_backend/data_tracking/viewsets.py
@@ -33,12 +33,9 @@ class IndicatorModelViewSet(ModelViewSet):
         date_filter_serializer = DateFilterRequestSerializer(data=self.request.query_params)
         date_filter_serializer.is_valid(raise_exception=True)
         time_filter = Q()
-        date = date_filter_serializer.validated_data.get('date')
         date_start = date_filter_serializer.validated_data.get('date_start')
         date_end = date_filter_serializer.validated_data.get('date_end')
-        if date:
-            time_filter = Q(measured_at__date=date)
-        elif date_start and date_end:
+        if date_start and date_end:
             time_filter = Q(measured_at__date__gte=date_start, measured_at__date__lte=date_end)
 
         return self.model_name.objects.filter(time_filter, user=self.request.user)
